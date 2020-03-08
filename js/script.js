71977b9644b6e462a917b6f4d7f79089ff7543f7
sohampatelk <sohampatelk@yahoo.com>
2020-03-07 19:49:27 -0700

    edit button just edded

--- js/script.js
+++ js/script.js
@@ -20,25 +20,38 @@ myForm.addEventListener("submit",function(event){
     //create button element with row -delete
     var deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
-
+    //add event lisner for -delete- button
     deleteButton.addEventListener("click",function(event){
         deleteTask(this);//delete the row
     });
-
     //create cell for delete button in table
     var deleteCell = document.createElement("td");
     deleteCell.appendChild(deleteButton);
     newRow.appendChild(deleteCell);
 
 
+
+    //create button element with row - Edit
+    var editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    //add event lisner for -Edit- button
+    editButton.addEventListener("click",function(event){
+        editTask(this);//delete the row
+    });
+    //create cell for -Edit- button in table
+    var editCell = document.createElement("td");
+    editCell.appendChild(editButton);
+    newRow.appendChild(editCell);
+
+
+
     //create button element with row - complete
     var compButton = document.createElement("button");
     compButton.textContent = "Complete";
-
+    //add event lisner for -Compaete- button
     compButton.addEventListener("click",function(event){
         completedTask(this);//delete the row
     });
-
     //create cell for complete button in table
     var compCell = document.createElement("td");
     compCell.appendChild(compButton);
@@ -48,7 +61,6 @@ myForm.addEventListener("submit",function(event){
     //append table row into new table body 
     var tableBody = document.querySelector("#table-body");
     tableBody.appendChild(newRow);
-
 
     //reset all element in table
     document.querySelector("#table-form").reset();
