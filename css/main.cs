5dc8506882a7c83f87c9f655d6905fd4ee20d6db
sohampatelk <sohampatelk@yahoo.com>
2020-03-07 17:58:38 -0700

    completed task finish

--- css/main.cs
+++ css/main.cs
@@ -1,11 +1,7 @@
-* {
+*{
     font-family: Arial, Helvetica, sans-serif;
     font-size: 16px;
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-}
-
-h1{
-
 }
--- js/script.js
+++ js/script.js
@@ -23,8 +23,7 @@ myForm.addEventListener("submit",function(event){
     compButton.textContent = "Complete";
 
     compButton.addEventListener("click",function(event){
-        addRowCompleteTable(this);
-        deleteRow(this);//delete the row
+        completedTask(this);//delete the row
     });
 
     //create cell for complete button in table
@@ -42,25 +41,34 @@ myForm.addEventListener("submit",function(event){
 });
 
 
-//grab existing buttons..which clicked
-var allButtons = document.querySelectorAll("#my-table>tbody>tr>td>button");
+/* //grab existing buttons..which clicked
+var allButtons = document.querySelectorAll("#table-body>tr>td>button");
+console.log(allButtons);
 //Loop through our query selected buttons,
 for(var i=0; i<allButtons.length ; i++){
     //lets add listerners to these!!
     allButtons[i].addEventListener("click",function(event){
-        addRowCompaleteTable(this);
+        //addRowCompaleteTable(this);
+        console.log(this.value);
         deleteRow(this);
     });
-}
+} */
 
 
-function addRowCompleteTable(element){
-
-
-}
 
 //Deleting row from to-do list table
-function deleteRow(element){//element will be button what we pressed it.
+function completedTask(element){//element will be button what we pressed it.
+    
     var tableRow = element.parentNode.parentNode;//2 levels up from bottom,to get our row.
-    tableRow.parentNode.removeChild(tableRow);//an element can not delete itself...so we havve to tell the parent to do it for us..
+    console.log(tableRow);
+    //select comleted dtable for append competed row
+    var compTableBody = document.querySelector("#comp-table-body");
+    console.log(compTableBody);
+    compTableBody.appendChild(tableRow);
+
+    //just print last child
+    console.log(tableRow.lastChild);
+    //remove button from table row because of task is completed.
+    tableRow.removeChild(tableRow.lastChild);
+    //tableRow.parentNode.removeChild(tableRow);//an element can not delete itself...so we havve to tell the parent to do it for us..
 }
